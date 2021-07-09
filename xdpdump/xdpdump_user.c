e501b9bb4a343bf13db1216eaa13d18d7d4a839f
laik <laik.lj@me.com>
2021-07-09 23:56:58 +0800

    xx

--- xdpdump/xdpdump_user.c
+++ xdpdump/xdpdump_user.c
@@ -1,29 +1,36 @@
 // SPDX-License-Identifier: (GPL-2.0 OR BSD-2-Clause)
 // Copyright (c) 2018 Netronome Systems, Inc.
 
+#include <arpa/inet.h>
 #include <assert.h>
+#include <bpf/bpf.h>
+#include <bpf/libbpf.h>
 #include <libgen.h>
+#include <linux/if_ether.h>
+#include <linux/if_link.h>
+#include <net/if.h>
 #include <perf-sys.h>
 #include <poll.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <string.h>
-#include <arpa/inet.h>
-#include <bpf/bpf.h>
-#include <bpf/libbpf.h>
-#include <linux/if_ether.h>
-#include <linux/if_link.h>
-#include <net/if.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
 #include <sys/sysinfo.h>
 
+#include <unistd.h>
+
+
 #include "xdpdump_common.h"
+
+extern char *optarg;
+extern int optind, opterr, optopt;
 
 #define NS_IN_SEC 1000000000
 #define PAGE_CNT 8
 
-struct perf_event_sample {
+struct perf_event_sample
+{
 	struct perf_event_header header;
 	__u64 timestamp;
 	__u32 size;
@@ -49,7 +56,8 @@ void meta_print(struct pkt_meta meta, __u64 timestamp)
 	char l3_str[32];
 	char l4_str[32];
 
-	switch (meta.l3_proto) {
+	switch (meta.l3_proto)
+	{
 	case ETH_P_IP:
 		strcpy(l3_str, "IP");
 		inet_ntop(AF_INET, &meta.src, src_str, INET_ADDRSTRLEN);
@@ -67,7 +75,8 @@ void meta_print(struct pkt_meta meta, __u64 timestamp)
 		sprintf(l3_str, "%04x", meta.l3_proto);
 	}
 
-	switch (meta.l4_proto) {
+	switch (meta.l4_proto)
+	{
 	case IPPROTO_TCP:
 		sprintf(l4_str, "TCP seq %d", ntohl(meta.seq));
 		break;
@@ -82,11 +91,11 @@ void meta_print(struct pkt_meta meta, __u64 timestamp)
 	}
 
 	printf("%lld.%06lld %s %s:%d > %s:%d %s, length %d\n",
-	       timestamp / NS_IN_SEC, (timestamp % NS_IN_SEC) / 1000,
-	       l3_str,
-	       src_str, ntohs(meta.port16[0]),
-	       dst_str, ntohs(meta.port16[1]),
-	       l4_str, meta.data_len);
+		   timestamp / NS_IN_SEC, (timestamp % NS_IN_SEC) / 1000,
+		   l3_str,
+		   src_str, ntohs(meta.port16[0]),
+		   dst_str, ntohs(meta.port16[1]),
+		   l4_str, meta.data_len);
 }
 
 int event_printer(struct perf_event_sample *sample)
@@ -95,9 +104,11 @@ int event_printer(struct perf_event_sample *sample)
 
 	meta_print(sample->meta, sample->timestamp);
 
-	if (dump_payload) { /* print payload hex */
+	if (dump_payload)
+	{ /* print payload hex */
 		printf("\t");
-		for (i = 0; i < sample->meta.pkt_len; i++) {
+		for (i = 0; i < sample->meta.pkt_len; i++)
+		{
 			printf("%02x", sample->pkt_data[i]);
 
 			if ((i + 1) % 16 == 0)
@@ -122,7 +133,7 @@ static enum bpf_perf_event_ret event_received(void *event, void *printfn)
 }
 
 int event_poller(struct perf_event_mmap_page **mem_buf, int *sys_fds,
-		 int cpu_total)
+				 int cpu_total)
 {
 	struct pollfd poll_fds[MAX_CPU];
 	void *buf = NULL;
@@ -133,25 +144,29 @@ int event_poller(struct perf_event_mmap_page **mem_buf, int *sys_fds,
 	int n;
 
 	/* Create pollfd struct to contain poller info */
-	for (n = 0; n < cpu_total; n++) {
+	for (n = 0; n < cpu_total; n++)
+	{
 		poll_fds[n].fd = sys_fds[n];
 		poll_fds[n].events = POLLIN;
 	}
 
 	pagesize = getpagesize();
 	total_size = PAGE_CNT * pagesize;
-	for (;;) {
+	for (;;)
+	{
 		/* Poll fds for events, 250ms timeout */
 		poll(poll_fds, cpu_total, 250);
 
-		for (n = 0; n < cpu_total; n++) {
-			if (poll_fds[n].revents) { /* events found */
+		for (n = 0; n < cpu_total; n++)
+		{
+			if (poll_fds[n].revents)
+			{ /* events found */
 				res = bpf_perf_event_read_simple(mem_buf[n],
-								 total_size,
-								 pagesize,
-								 &buf, &len,
-								 event_received,
-								 event_printer);
+												 total_size,
+												 pagesize,
+												 &buf, &len,
+												 event_received,
+												 event_printer);
 				if (res != LIBBPF_PERF_EVENT_CONT)
 					break;
 			}
@@ -161,13 +176,13 @@ int event_poller(struct perf_event_mmap_page **mem_buf, int *sys_fds,
 }
 
 int setup_perf_poller(int perf_map_fd, int *sys_fds, int cpu_total,
-		      struct perf_event_mmap_page **mem_buf)
+					  struct perf_event_mmap_page **mem_buf)
 {
 	struct perf_event_attr attr = {
-		.sample_type	= PERF_SAMPLE_RAW | PERF_SAMPLE_TIME,
-		.type		= PERF_TYPE_SOFTWARE,
-		.config		= PERF_COUNT_SW_BPF_OUTPUT,
-		.wakeup_events	= 1,
+		.sample_type = PERF_SAMPLE_RAW | PERF_SAMPLE_TIME,
+		.type = PERF_TYPE_SOFTWARE,
+		.config = PERF_COUNT_SW_BPF_OUTPUT,
+		.wakeup_events = 1,
 	};
 	int mmap_size;
 	int pmu;
@@ -175,10 +190,12 @@ int setup_perf_poller(int perf_map_fd, int *sys_fds, int cpu_total,
 
 	mmap_size = getpagesize() * (PAGE_CNT + 1);
 
-	for (n = 0; n < cpu_total; n++) {
+	for (n = 0; n < cpu_total; n++)
+	{
 		/* create perf fd for each thread */
 		pmu = sys_perf_event_open(&attr, -1, n, -1, 0);
-		if (pmu < 0) {
+		if (pmu < 0)
+		{
 			printf("error setting up perf fd\n");
 			return 1;
 		}
@@ -187,8 +204,9 @@ int setup_perf_poller(int perf_map_fd, int *sys_fds, int cpu_total,
 
 		/* give fd a memory buf to write to */
 		mem_buf[n] = mmap(NULL, mmap_size, PROT_READ | PROT_WRITE,
-				  MAP_SHARED, pmu, 0);
-		if (mem_buf[n] == MAP_FAILED) {
+						  MAP_SHARED, pmu, 0);
+		if (mem_buf[n] == MAP_FAILED)
+		{
 			printf("error creating mmap\n");
 			return 1;
 		}
@@ -202,14 +220,14 @@ int setup_perf_poller(int perf_map_fd, int *sys_fds, int cpu_total,
 static void usage(const char *prog)
 {
 	fprintf(stderr,
-		"%s -i interface [OPTS]\n\n"
-		"OPTS:\n"
-		" -h        help\n"
-		" -H        Hardware Mode (XDPOFFLOAD)\n"
-		" -N        Native Mode (XDPDRV)\n"
-		" -S        SKB Mode (XDPGENERIC)\n"
-		" -x        Show packet payload\n",
-		prog);
+			"%s -i interface [OPTS]\n\n"
+			"OPTS:\n"
+			" -h        help\n"
+			" -H        Hardware Mode (XDPOFFLOAD)\n"
+			" -N        Native Mode (XDPDRV)\n"
+			" -S        SKB Mode (XDPGENERIC)\n"
+			" -x        Show packet payload\n",
+			prog);
 }
 
 int main(int argc, char **argv)
@@ -231,13 +249,16 @@ int main(int argc, char **argv)
 	n_cpus = get_nprocs();
 	dump_payload = 0;
 
-	if (optind == argc) {
+	if (optind == argc)
+	{
 		usage(basename(argv[0]));
 		return -1;
 	}
 
-	while ((opt = getopt(argc, argv, "hHi:NSx")) != -1) {
-		switch (opt) {
+	while ((opt = getopt(argc, argv, "hHi:NSx")) != -1)
+	{
+		switch (opt)
+		{
 		case 'h':
 			usage(basename(argv[0]));
 			return 0;
@@ -264,18 +285,21 @@ int main(int argc, char **argv)
 		}
 	}
 
-	if (ifindex == 0) {
+	if (ifindex == 0)
+	{
 		printf("error, invalid interface\n");
 		return -1;
 	}
 
 	/* use libbpf to load program */
-	if (bpf_prog_load_xattr(&prog_load_attr, &obj, &prog_fd)) {
+	if (bpf_prog_load_xattr(&prog_load_attr, &obj, &prog_fd))
+	{
 		printf("error with loading file\n");
 		return -1;
 	}
 
-	if (prog_fd < 1) {
+	if (prog_fd < 1)
+	{
 		printf("error creating prog_fd\n");
 		return -1;
 	}
@@ -284,7 +308,8 @@ int main(int argc, char **argv)
 	signal(SIGTERM, unload_prog);
 
 	/* use libbpf to link program to interface with corresponding flags */
-	if (bpf_set_link_xdp_fd(ifindex, prog_fd, xdp_flags) < 0) {
+	if (bpf_set_link_xdp_fd(ifindex, prog_fd, xdp_flags) < 0)
+	{
 		printf("error setting fd onto xdp\n");
 		return -1;
 	}
@@ -292,7 +317,8 @@ int main(int argc, char **argv)
 	perf_map = bpf_object__find_map_by_name(obj, "perf_map");
 	perf_map_fd = bpf_map__fd(perf_map);
 
-	if (perf_map_fd < 0) {
+	if (perf_map_fd < 0)
+	{
 		printf("error cannot find map\n");
 		return -1;
 	}
